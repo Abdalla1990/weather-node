57cae823d4ef7d17a29764c249b39046a746dd77
Abdalla1990 <abdalla.ahmed.ksa@gmail.com>
2017-09-30 19:34:01 -0400

    simplifies the app to access my call backs using a library called AXIOS which makes a default promis integration with an automatically json type object response !

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
- const yargs = require('yargs');
- const geocode = require('./geocode/geocode.js');
- const forcast = require('./forecast/forecast.js')
- const argv = yargs.
- options({
-         a: { // the fied a 
-             demand: true, // required field 
-             describe: 'enter your address ', // will be displayed in the help command 
-             alias: 'address', // alies of a 
-             string: true // will transfer the inserted text into string data type 
-         }
-     }).help() // needed to provide guidance 
-     .alias('help', 'h') // alies for help 
-     .argv;
+const yargs = require('yargs');
+const axios = require('axios');
+const argv = yargs.
+options({
+        a: { // the fied a 
+            demand: true, // required field 
+            describe: 'enter your address ', // will be displayed in the help command 
+            alias: 'address', // alies of a 
+            string: true // will transfer the inserted text into string data type 
+        }
+    }).help() // needed to provide guidance 
+    .alias('help', 'h') // alies for help 
+    .argv;
 
- geocode.getaddress(argv.address).then((results) => {
-     console.log('getting the lat and grat and sending it to the forcasting ...')
 
-     console.log(`================================`);
-     console.log(`address   :  ${results.address}`);
-     console.log(`latitude  :  ${results.latitude}`);
-     console.log(`gratitude :  ${results.gratitude}`);
+var encodedAddress = encodeURIComponent(argv.address);
+var url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
 
-     forcast.sendForcast(results.latitude, results.gratitude).then((result) => {
+axios.get(url).then((response) => {
 
-         console.log(`it is now ${result.temperature} and it feels like ${result.apparentTemperature}`);
-         console.log(`================================`);
+    if (response.data.status === 'ZERO_RESULTS') { throw new Error('Unable to find that address. ') }
+    console.log(`================================`);
+    console.log(`address      :  ${response.data.results[0].formatted_address}`);
+    console.log(`latitude     :  ${response.data.results[0].geometry.location.lat}`);
+    console.log(`langtitude   :  ${response.data.results[0].geometry.location.lng}`);
 
-     }, (errorMessage) => {
-         console.log(errorMessage);
-     });
- });
+    var url2 = `https://api.darksky.net/forecast/90ed774613b6cd56ad3b3078a7350af6/${response.data.results[0].geometry.location.lat},${response.data.results[0].geometry.location.lng}`; // getting the encoded address 
+
+
+    return axios.get(url2);
+
+
+}).then((response1) => {
+
+    console.log(`it is now ${response1.data.currently.temperature} and it feels like ${response1.data.currently.apparentTemperature}`);
+    console.log(`================================`);
+
+
+}).catch((error) => {
+
+    if (error.code === 'ENOTFOUND')
+        console.log(`unable to coonect to API ${error.code}`);
+    else
+        console.log(`there is an error ${error.message}`);
+});
